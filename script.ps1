d8fb5516e3c5b9d28d5f88c10b02287f773b7002
Luciano Ferruzzi <ferruzzi.luciano@gmail.com>
2020-10-12 18:37:16 -0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,13 +1,10 @@
-Add-Type -AssemblyName System.Windows.Forms
+$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3'
+$v=(Get-ItemProperty -Path $p).Settings
 
-$scrollLockKeyStatus = [System.Windows.Forms.Control]::IsKeyLocked('Scroll')
-
-$keyBoardObject = New-Object -ComObject WScript.Shell 
-
-if ($scrollLockKeyStatus -eq $false )  {
-   $keyBoardObject.SendKeys("{SCROLLLOCK}") 
-   &{$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=3;&Set-ItemProperty -Path $p -Name Settings -Value $v}
+if ($v[8] -eq 2) {
+   $v[8]=3
+   &Set-ItemProperty -Path $p -Name Settings -Value $v
 } else {
-    $keyBoardObject.SendKeys("{SCROLLLOCK}") 
-    &{$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=2;&Set-ItemProperty -Path $p -Name Settings -Value $v}
+   $v[8]=2
+   &Set-ItemProperty -Path $p -Name Settings -Value $v
 }
