ee295fbf6023ac7a9752114271775ec5bdec5be4
Luciano Ferruzzi <ferruzzi.luciano@gmail.com>
2020-10-12 01:59:41 -0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -6,8 +6,8 @@ $keyBoardObject = New-Object -ComObject WScript.Shell
 
 if ($scrollLockKeyStatus -eq $false )  {
    $keyBoardObject.SendKeys("{SCROLLLOCK}") 
-   &{$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=3;&Set-ItemProperty -Path $p -Name Settings -Value $v;&Stop-Process -f -ProcessName explorer}
+   &{$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=3;&Set-ItemProperty -Path $p -Name Settings -Value $v}
 } else {
     $keyBoardObject.SendKeys("{SCROLLLOCK}") 
-    &{$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=2;&Set-ItemProperty -Path $p -Name Settings -Value $v;&Stop-Process -f -ProcessName explorer}
+    &{$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=2;&Set-ItemProperty -Path $p -Name Settings -Value $v}
 }
