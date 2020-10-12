a10f9507f8417cf2c58bf81165ebe3ab7f722d09
Luciano Ferruzzi <ferruzzi.luciano@gmail.com>
2020-10-12 00:25:31 -0300

    Create script.ps1

---
+++ script.ps1
@@ -0,0 +1,13 @@
+Add-Type -AssemblyName System.Windows.Forms
+
+$scrollLockKeyStatus = [System.Windows.Forms.Control]::IsKeyLocked('Scroll')
+
+$keyBoardObject = New-Object -ComObject WScript.Shell 
+
+if ($scrollLockKeyStatus -eq $false )  {
+   $keyBoardObject.SendKeys("{SCROLLLOCK}") 
+   &{$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=3;&Set-ItemProperty -Path $p -Name Settings -Value $v;&Stop-Process -f -ProcessName explorer}
+} else {
+    $keyBoardObject.SendKeys("{SCROLLLOCK}") 
+    &{$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=2;&Set-ItemProperty -Path $p -Name Settings -Value $v;&Stop-Process -f -ProcessName explorer}
+}
